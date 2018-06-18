2de1c2f4ea3afff5aac903745184cd9bfd407d2e
Owain Williams <owaingdwilliams@gmail.com>
2018-06-18 22:15:07 +0100

    More tutorials
    Working my way through the udemy tutorials.

---
+++ Owain/06/classesDemo/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ConsoleApp1;
+using ConsoleApp1.Maths;
+
+namespace ConsoleApp1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Williams";
+            person.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(2, 4);
+
+
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
